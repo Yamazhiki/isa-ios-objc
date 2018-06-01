7a295d51efc47360aaf3788aba248e8a7cd0dfba
Yamazhiki <Yamazhiki@gmail.com>
2018-06-01 13:28:31 +0800

    抽出POP 网络库为YZKNetwork

---
+++ YZKNetWorkTests/YZKNetWorkTests.m
@@ -0,0 +1,39 @@
+//
+//  YZKNetWorkTests.m
+//  YZKNetWorkTests
+//
+//  Created by Yamazhiki on 01/06/2018.
+//  Copyright © 2018 Yamazhiki. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YZKNetWorkTests : XCTestCase
+
+@end
+
+@implementation YZKNetWorkTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
