3e7d488258e0cc59a2c81e64d6da05e436c194dc
your name <your email>
2020-05-29 22:14:22 +0800

    add a log

--- Crocodile/AppDelegate.m
+++ Crocodile/AppDelegate.m
@@ -19,6 +19,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"123");
     return YES;
 }
 
